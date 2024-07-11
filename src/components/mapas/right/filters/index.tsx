a0f7c9915856ab2cfc62426fb6d3226b7d97add6
hvoking <gus@ugeom.com>
2024-07-11 02:14:35 -0400

    backup 11-07-24

--- src/components/mapas/right/filters/index.tsx
+++ src/components/mapas/right/filters/index.tsx
@@ -1,13 +1,39 @@
 // App imports
-import { Property } from './property';
+import { Rooms } from './rooms';
+import { Dropdown } from './dropdown';
 import './styles.scss';
 
+// Context imports
+import { useProperty } from '../../context/filters/property';
+
 export const Filters = () => {
+	const  { propertyName, businessName, setBusinessTypeId, setPropertyTypeId, propertyTypeDict, businessTypeDict } = useProperty();
+
 	return (
 		<div className="stats-wrapper">
 			<div className="stats">
 				<div className="stats-title">Filtros</div>
-				<Property/>
+				<div className="property-parameters">
+					<div style={{display: "flex", justifyContent: "space-between"}}>
+						<div className="property-parameter">
+							<div>Tipo:</div>
+							<Dropdown
+								imoveisDict={propertyTypeDict}
+								propertyName={propertyName}
+								setPropertyTypeId={setPropertyTypeId}
+							/>
+						</div>
+						<div className="property-parameter">
+							<div>Negócio:</div>
+							<Dropdown
+								imoveisDict={businessTypeDict}
+								propertyName={businessName}
+								setPropertyTypeId={setBusinessTypeId}
+							/>
+						</div>
+					</div>
+					<Rooms/>
+				</div>
 			</div>
 		</div>
 	)
